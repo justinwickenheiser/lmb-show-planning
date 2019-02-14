7b97750fde488eb0c3c028143a0c20d864d34026
Justin Wickenheiser <wickenhj@gmail.com>
2019-02-14 15:54:06 -0500

    header/footer/start of nav'start of routing

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Header from './components/header.jsx';
+import Footer from './components/footer.jsx';
+import Nav from './components/nav.jsx';
+import Home from './components/home.jsx';
+import Library from './components/library.jsx';
+import Shows from './components/shows.jsx';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+// Styles
 import './App.css';
+import './css/styles.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+	return (
+	<div className="App">
+		<Header />
+		<Nav />
+
+		<Router>
+			<div>
+				<Route exact path="/" component={Home} />
+				<Route path="/library" component={Library} />
+				<Route path="/shows" component={Shows} />
+			</div>
+		</Router>
+
+		<Footer />
+	</div>
+	);
   }
 }
 
---
+++ src/components/header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Header(props) {
+	return (
+		<div className="header-text">
+			<div className="headerTitle">
+				<h1>Laker Marching Band</h1>
+			</div>
+			<div className="headerSubTitle">
+				<h2>Show Planner</h2>
+			</div>
+		</div>
+	);
+}
